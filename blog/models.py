47bfb8143f6d2d538c2c188e878ae9992c9e069e
isaccchoi <isaccchoi@naver.com>
2017-07-23 21:38:10 +0900

    add get_absoulte_url to post model

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.core.exceptions import ValidationError
 from django.db import models
-# Create your models here.
+from django.urls import reverse
 from django.conf import settings
+
+def min_length_3_validator(value):
+    if len(value) < 3:
+        raise ValidationError('3글자 이상 입력해주세요.')
 
 class Post(models.Model):
     author = models.CharField(max_length=30)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, validators=[min_length_3_validator], help_text="3글자 이상 입력하세요.")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -17,3 +22,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
