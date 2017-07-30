3d937fc050e8951b802fa33066dda6aa7ea8cd04
isaccchoi <isaccchoi@naver.com>
2017-07-30 22:12:17 +0900

    edit forms suitably(can deal with image)

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,8 @@ from . import views_cbv
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^new/$', views_cbv.post_new, name='post_new'),
-    url(r'^(?P<pk>\d+)/edit/$', views_cbv.post_edit, name='post_edit'),
+    url(r'^new/$', views.post_new, name='post_new'),
+    url(r'^(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
     # url(r'^new/$', views.post_new, name='post_new'),
     # url(r'^(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
--- blog/views.py
+++ blog/views.py
@@ -25,7 +25,7 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save()
             return redirect(post)  #get_absolute_url로 이동
@@ -38,7 +38,7 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save()
             return redirect('blog:post_detail', post.id)
