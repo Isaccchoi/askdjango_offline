d8bf1823489c06690f39af911fddad60357e877b
isaccchoi <isaccchoi@naver.com>
2017-08-03 08:41:46 +0900

    add signup form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'profile/$', views.profile, name='profile'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
